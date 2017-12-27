08c3a65bc7e02c173cef628d6b4a5af3b76b6d6b
nancy sin <nsin@sbcglobal.net>
2017-12-27 01:34:30 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -17,7 +17,16 @@ class Question{
   constructor(content){
     this.content = content
   }
-
+ insert(){
+   const self = this
+   const. sql = `INSERT INTO questions (content) VALUES (?)`
+   return new Promise(function(resolve){
+     db.run (sql  [self.content], function (err, result){
+       self.id = this.lastID
+      resolve("Row Inserted!")
+    })
+   })
+ }
 }
 
 module.exports = Question;
